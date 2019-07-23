c16accc9bed8922a71c14863fec79d8e53322155
P1start <rewi-github@whanau.org>
2019-07-23 16:08:39 +1200

    Switch the alternate time system to ISO week date + normal time

--- pkgs/polybar-modules/src/date.py
+++ pkgs/polybar-modules/src/date.py
@@ -21,34 +21,23 @@ if len(sys.argv) > 1:
         f.write(str((mode + 1) % 2))
 
 def card_character(suit, value):
+    if suit == 4:
+        return '🃟'
     if value > 10:
         value += 1
     return chr(0x1F0A1 + 16*suit + value)
 
 weekdays = ['☾', '♂', '☿', '♃', '♀', '♄', '%{T6}☉%{T1}']
-suits = '♠♡♢♣'
-values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 def name(day):
-    acc = list(itertools.accumulate([0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]))
+    year, week, weekday = day.isocalendar()
+    week -= 1
+    weekday -= 1
 
-    # Day of year, starting from zero
-    day_of_year = acc[day.month - 1] + day.day - 1
-
-    if day_of_year == 364:
-        return "🃟"
-        #return "♕"
-    elif day_of_year == 365:
-        return "🃏"
-        #return "♛"
-
-    week = day_of_year // 7
-    weekday = day_of_year % 7
     value = week % 13
     suit = week // 13
 
-    return str(day.year) + ' %{T1}' + card_character(suit, value) + '%{T1}' + weekdays[weekday]
-    #return str(day.year) + ' %{T13}3' + '%{T1}' + values[value] + ' ' + weekdays[weekday]
+    return str(year) + ' %{T1}' + card_character(suit, value) + '%{T1}' + weekdays[weekday]
 
 months = ['%{T3}♑', '%{T3}♒', '%{T3}♓', '%{T3}♈', '%{T6}♉', '%{T3}♊', '%{T6}♋', '%{T3}♌', '%{T5}♍', '%{T3}♎', '%{T3}♏', '%{T6}♐']
 def symbolic_name(day):
@@ -58,20 +47,10 @@ def symbolic_name(day):
 def decimal_time(t):
     return (t.hour*3600 + t.minute*60 + t.second + t.microsecond*1e-6) / 3600 / 24
 
-def natural_time():
-    from ephemeris import time_of_day
-    section, fraction = time_of_day()
-    return f"{section} {math.floor(fraction*100):.0f}%"
-
 def cool_time():
-    # Days start approximately at sunrise, years start approximately on 'December' solstice (December 21)
-    now = datetime.datetime.now() + datetime.timedelta(days=11, hours=-6)
-    today = now.date()
-    return name(today) + '%{T1} ' + natural_time()#'%{T1} ' + '{:.04f}'.format(decimal_time(now.time()))[2:]
-
-def iso_time():
     now = datetime.datetime.now()
-    return now.strftime('%Y-%m-%d %H:%M')
+    today = now.date()
+    return name(today) + '%{T1} ' + now.strftime("%H%M")
 
 def symbolic_time():
     now = datetime.datetime.now()
@@ -81,5 +60,3 @@ if mode == 0:
     print(cool_time())
 elif mode == 1:
     print(symbolic_time())
-elif mode == 2:
-    print(iso_time())
